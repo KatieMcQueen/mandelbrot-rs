23796c2a48da296f70bec1aa17035d8c92a3ece3
Katelyn McQueen <54079638+KatieMcQueen@users.noreply.github.com>
2024-07-04 15:18:58 -0700

    initial arbitrary scaling tests

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,11 @@ fn main() {
     let colors = build_colormap("data/color/bamO/bamO.lut").unwrap();
     println!("Built!");
 
-    let zoom: f64 = 1;
-    let center: Complex = Complex::new(0,0);
+    let zoom: f64 = 2.0;
+    let center: Complex<f64> = Complex::new(0.0,0.0);
+    let (width, height) = img.dimensions();
+
+    let zoom = 2.0 * (1.0 / zoom);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         //this code needs to be refactored elsewhere
@@ -19,8 +22,8 @@ fn main() {
         //convert width to real component
         //convert hight to imaginary component
         //build complex representation
-        let re = (x as f64 / w as f64 -0.5) * 2.0;
-        let im = (y as f64 / h as f64 -0.5) * 2.0;
+        let re = (x as f64 / w as f64 -0.5) * zoom + center.re;
+        let im = (y as f64 / h as f64 -0.5) * zoom + center.im;
         let point = Complex::new(re, im);
         
         match iterations(255, point) {
